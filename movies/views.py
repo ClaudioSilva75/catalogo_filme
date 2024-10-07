801c7dbc4b63c89695b95fb8c79941308d839486
CLAUDIO DA SILVA <66528978+ClaudioSilva75@users.noreply.github.com>
2024-10-07 17:55:02 -0300

    URLs Views e Templates - Parte II
    Ajustando as views e incluindo Boostrap e CSS.

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,23 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from movies.models import Movie
+from genres.models import Genre
 
 
 class MoviesListView(ListView):
     model = Movie
     template_name = 'base.html'
     context_object_name = 'movies' # objeto que irá para template
+
+    def queryset(self):
+        queryset = Genre.objects.prefetch_related('movies').all()
+        search = self.request.GET.get('search')
+
+        if search:
+            queryset = queryset.filter(movies__title__icontains=search).distinct()
+
+        return queryset
+
 
 class MoviesDetailView(DetailView):
     model = Movie
