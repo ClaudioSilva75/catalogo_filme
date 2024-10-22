62e7549bdbfa93b71fa5f41699360d1b2e6fdbc3
CLAUDIO DA SILVA <66528978+ClaudioSilva75@users.noreply.github.com>
2024-10-22 19:22:10 -0300

    Correção do bug, page home.

--- app/settings.py
+++ app/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,14 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from movies.models import Movie
+from django.views.generic import DetailView, ListView
+
 from genres.models import Genre
+from movies.models import Movie
 
 
 class MoviesListView(ListView):
     model = Movie
     template_name = 'movies/movies_list.html'
-    context_object_name = 'movies' # objeto que irá para template
+    context_object_name = 'genres' # objeto que irá para template
 
     def queryset(self):
         queryset = Genre.objects.prefetch_related('movies').all()
@@ -23,4 +24,3 @@ class MoviesDetailView(DetailView):
     model = Movie
     template_name = 'movies/movie_detail.html'
     context_object_name = 'movie'
-    
